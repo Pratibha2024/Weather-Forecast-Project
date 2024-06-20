f80b1168f892608fd0d8947de9f35c0d7012ef8e
Pratibha2024 <bpratibha805@gmail.com>
2024-06-20 23:04:32 +0530

    Making code responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': {'min': '375px', 'max': '667px'}, // iPhone SE media query
+        'md': { 'min': '668px', 'max': '1023px' }, // ipad mini media query
+        'lg': {'min': '1024px', 'max': '1280px' }, // desktop media query
+      },
+    },
   },
   plugins: [],
 }
